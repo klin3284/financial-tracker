817eee1103f2fc89e3ae6a089326f6cdd33f9933
Kenny <lin.kenn@northeastern.edu>
2024-09-21 03:01:00 -0400

    shadcn latest initial setup

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from 'next';
 import React from 'react';
+import localFont from 'next/font/local';
 import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,8 +26,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
