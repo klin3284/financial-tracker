cf261fbd235a47dff4e5c5fba2a654d24f31e036
Kenny <lin.kenn@northeastern.edu>
2024-09-21 22:59:37 -0400

    remove footer from auth pages

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -1,19 +1,27 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-const Footer = () => (
-  <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t'>
-    <p className='text-xs text-gray-500 dark:text-gray-400'>
-      © 2024 Finance Tracker. All rights reserved.
-    </p>
-    <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
-      <Link className='text-xs hover:underline underline-offset-4' href='#'>
-        Terms of Service
-      </Link>
-      <Link className='text-xs hover:underline underline-offset-4' href='#'>
-        Privacy
-      </Link>
-    </nav>
-  </footer>
-);
+const Footer = () => {
+  const isAuthPage = ['/sign-in', '/sign-up'].includes(usePathname());
 
+  if (isAuthPage) return null;
+
+  return (
+    <footer className='flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t'>
+      <p className='text-xs text-gray-500 dark:text-gray-400'>
+        © 2024 Finance Tracker. All rights reserved.
+      </p>
+      <nav className='sm:ml-auto flex gap-4 sm:gap-6'>
+        <Link className='text-xs hover:underline underline-offset-4' href='#'>
+          Terms of Service
+        </Link>
+        <Link className='text-xs hover:underline underline-offset-4' href='#'>
+          Privacy
+        </Link>
+      </nav>
+    </footer>
+  );
+};
 export default Footer;
